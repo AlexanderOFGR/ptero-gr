97e950b0a725db07b779b5fb6bca459d01ef5ac5
AlexanderOF <58230188+AlexanderOFGR@users.noreply.github.com>
2020-09-07 01:36:15 +0300

    Add files via upload

---
+++ navigation.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'home' => 'Home',
+    'account' => [
+        'header' => 'ΔΕΙΑΧΕΙΡΗΣΗ ΛΟΓΑΡΙΑΣΜΟΥ',
+        'my_account' => 'Ο λογαριασμός μου',
+        'security_controls' => 'Έλεγχοι ασφαλείας',
+        'api_access' => 'API Λογαριασμού',
+        'my_servers' => 'Οι servers μου',
+    ],
+    'server' => [
+        'header' => 'ΔΙΑΧΕΙΡΙΣΗ SERVER',
+        'console' => 'Κονσόλα',
+        'console-pop' => 'Κοσνόλα πλήρους οθόνης',
+        'file_management' => 'Διαχείριση Αρχείων',
+        'file_browser' => 'File Browser',
+        'create_file' => 'Δημιουργία Αρχείου',
+        'upload_files' => 'Ανέβασμα Αρχείου',
+        'subusers' => 'Άλλοι Χρήστες',
+        'schedules' => 'Σχέδια',
+        'configuration' => 'Ρυθμίσεις',
+        'port_allocations' => 'Ρυθμίσεις κατανομών',
+        'sftp_settings' => 'Ρυθμίσεις SFTP',
+        'startup_parameters' => 'Παράμετροι εκκίνησης',
+        'databases' => 'Βάσεις Δεδομένων',
+        'edit_file' => 'Επεξεργασία Αρχείου',
+        'admin_header' => 'ADMINISTRATIVE',
+        'admin' => 'ΔΙΑΧΕΙΡΙΣΗ',
+        'server_name' => 'Ονομασία Server',
+    ],
+];
---
+++ passwords.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+    'password' => 'Ο κωδικός πρέπει να είναι τουλάχιστον 6 χαρακτήρες και να ταιριάζει με την επιβεβαίωση.',
+    'reset' => 'Ο κωδικός του λογαριασμού σου άλλαξε με επιτυχία!',
+    'sent' => 'Στάλθηκε ο σύνδεσμος για την επαναφορά κωδικού στο email σας',
+    'token' => 'Το token για την επαναφορά κωδικού είναι λανθασμένο',
+    'user' => "Δεν μπορέσαμε να βρούμε κάποιον λογαριασμό με αυτή την διεύθυνση email",
+];
